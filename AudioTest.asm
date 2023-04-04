b4be0be014c84038542f42d6cfde96deb2239258
ic-1234 <113953073+ic-1234@users.noreply.github.com>
2023-04-04 16:23:53 -0400

    Update AudioTest.asm
    Base functionality of the hex display - increments the clap count and displays on the 4-digit hex; displays the current threshold on the 2-digit one

--- AudioTest.asm
+++ AudioTest.asm
@@ -1,35 +1,35 @@
 ; AudioTest.asm
-; Displays the value from the audio peripheral
+; Reads in the output from the peripheral and displays both the count and the threshold level on the hex display
 
 ORG 0
+; The lower 4 bits of the audio peripheral output will be incrementing the number of claps by either 1 or 0; the next 4 bits will be the threshold level (1-3)
+Init:
 	; Get data from the audio peripheral
 	IN     Sound
-	; Display value on Hex 0
-	OUT    Hex0
-    ; Display most-significant 10 bits of the magnitude on LEDs
-    CALL   Abs
-    SHIFT  -6
-    OUT    LEDs
-	; Do it again
+	STORE	Data	; Store the OG data
+	; Display most-significant 10 bits of the magnitude on LEDs
+	AND	Mask
+	ADD	Count
+	STORE	Count
+	OUT	Hex0
+	LOAD	Data
+	SHIFT	-4
+	STORE 	Data
+	AND	Mask
+	OUT	Hex1
+	; GO to the start of the program
 	JUMP   0
-	
-; Subroutine to get the absolute value.
-; Additional subroutine for negation is made available,
-; since that's required for absolute value anyway.
-Abs:
-	JNEG   Negate
-    RETURN ; positive or zero; just return
-Negate:
-	STORE  NegTemp
-    SUB    NegTemp
-    SUB    NegTemp
-	RETURN
-NegTemp: DW 0
+
+; Variables
+Count: DW 0
+Data:	DW 0
+Mask:	DW &H0F
+ResetSwitch:	DW 9
 
 ; IO address constants
 Switches:  EQU 000
 LEDs:      EQU 001
 Timer:     EQU 002
-Hex0:      EQU 004
-Hex1:      EQU 005
+Hex0:      EQU 004 ; The one with 4 digits
+Hex1:      EQU 005 ; The one with 2 digits
 Sound:     EQU &H50
