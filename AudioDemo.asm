2ba762d39c6a37b6a4e7633355268c1eb9a64640
Levi Tucker <levitucker01@gmail.com>
2023-04-15 14:33:21 -0400

    Update AudioDemo.asm

--- AudioDemo.asm
+++ AudioDemo.asm
@@ -1,18 +1,25 @@
 ; AudioDemo.asm
-; demonstrates function of the peripheral!
+; Created 2023
+; Levi Tucker, George Lee, Edmond Li, Isaac Chia, Edward Kwak
+; Demonstrates functionality of the AudioMonitor peripheral.
 
 ORG 0
 
 Init:
+	; get threshold as hex data from switches
 	IN Switches
 	STORE Threshold
 	
+	; display threshold level on 2-digit hex
 	LOAD Threshold
 	OUT	Hex1
 	OUT Sound
     
+	; delay to demonstrate SCOMP doesn't need to constantly
+	; poll the peripheral for clap data
 	CALL Delay
 	
+	; get clap data from peripheral and display on 4-digit hex
     IN Sound
     OUT Hex0
     
