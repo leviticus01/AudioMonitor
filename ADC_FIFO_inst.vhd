3dfdbfe3467086abd584769eaf37a0d1aca5ebb0
Levi Tucker <levitucker01@gmail.com>
2023-04-15 14:22:20 -0400

    Delete ADC_FIFO_inst.vhd

--- ADC_FIFO_inst.vhd
+++
@@ -1,9 +0,0 @@
-ADC_FIFO_inst : ADC_FIFO PORT MAP (
-		data	 => data_sig,
-		rdclk	 => rdclk_sig,
-		rdreq	 => rdreq_sig,
-		wrclk	 => wrclk_sig,
-		wrreq	 => wrreq_sig,
-		q	 => q_sig,
-		rdempty	 => rdempty_sig
-	);
